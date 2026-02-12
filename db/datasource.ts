f411b371cdbab170bd44ec393e511037da2b8c0e
OmarLawaty <omarlawatey@gmail.com>
2026-02-12 04:58:15 +0200

    feat: Add migration for production

--- db/datasource.ts
+++ db/datasource.ts
@@ -4,6 +4,7 @@ export const getDataSourceOptions = (): DataSourceOptions => {
   const dataSourceOptions: Partial<DataSourceOptions> = {
     synchronize: false,
     migrations: [__dirname + '/migrations/*.{ts,js}'],
+    entities: [__dirname + '/../src/**/*.entity.{ts,js}'],
   };
 
   switch (process.env.NODE_ENV) {
@@ -11,14 +12,12 @@ export const getDataSourceOptions = (): DataSourceOptions => {
       Object.assign(dataSourceOptions, {
         type: 'sqlite',
         database: 'db.sqlite',
-        entities: [__dirname + '/../src/**/*.entity.{ts,js}'],
       });
       break;
     case 'test':
       Object.assign(dataSourceOptions, {
         type: 'sqlite',
         database: 'test.sqlite',
-        entities: [__dirname + '/../src/**/*.entity.ts'],
         migrationsRun: true,
       });
       break;
@@ -26,7 +25,6 @@ export const getDataSourceOptions = (): DataSourceOptions => {
       Object.assign(dataSourceOptions, {
         type: 'postgres',
         url: process.env.DATABASE_URL,
-        entities: [__dirname + '/../src/**/*.entity.js'],
         migrationsRun: true,
         ssl: {
           rejectUnauthorized: false,
